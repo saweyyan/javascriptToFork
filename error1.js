d270e5f89a28d3211f01d60f988a0a1a3f1a8a7d
saweyyan <87162289+saweyyan@users.noreply.github.com>
2021-08-11 07:59:10 +0300

    practise variables

--- error1.js
+++ error1.js
@@ -1,4 +1,24 @@
+let a = 6 
+a = 7 
+console.log('a value is ',a) 
+
+let b 
+console.log('b is ',b)
+b = 7 
+console.log('b is ',b)
+
+
 const x = 5
 x = 4 
+console.log('x is ',x) 
 
-console.log(x) 
+const d
+d = 7
+console.log('d value is ',d) 
+
+
+console.log('y is ',y) 
+
+// run the code , then comment the code that causing the error with explain short words , then fix the code 
+// * to comment the code add // in the start of the line or wrap the code with /* your code here */
+
