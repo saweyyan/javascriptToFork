e85abab67cfbef19e5553c694b1118639a9905c4
saweyyan <87162289+saweyyan@users.noreply.github.com>
2021-08-11 08:51:28 +0300

    add array and object exercise

---
+++ exercise.js
@@ -0,0 +1,13 @@
+// create this list of players and store them in variable name players
+/*
+name  age   rating 
+a     23      5
+b     22      3
+c     22      3
+*/
+
+
+// your code 
+const players = 
+// write single line code to print the 2nd player name in the list ( without using dot notation .name )
+console.log(.............)
