ded9c9c10e050970c9640b5a1aed4303cc4ca716
Yahya Mustafa <yahya_odeh@outlook.com>
2021-08-11 12:15:29 +0300

    Solved

--- exercise.js
+++ exercise.js
@@ -6,8 +6,26 @@ b     22      3
 c     22      3
 */
 
+let players = [
 
-// your code 
-const players = 
-// write single line code to print the 2nd player name in the list ( without using dot notation .name )
-console.log(.............)
+    ['a','23','5'],
+    ['b','22','3'],
+    ['c','22','3']
+    
+    ]
+    
+    
+    // Alternative solution
+    
+    let PlayerA='a'
+    let PlayerB='b'
+    let PlayerC='c'
+    
+    
+    let players2 = [PlayerA,PlayerB,PlayerC]
+    
+     
+    // write single line code to print the 2nd player name in the list ( without using dot notation .name )
+    console.log(players[1][0])
+    console.log(players2[1])
+    
