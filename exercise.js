5b1c3b7afd72c168eca4e32755b0e8bcc9786711
Omar <omar.ajaj@saweyyan.com>
2021-08-12 21:26:16 +0300

    Fixed the Errors and Did the Example

--- exercise.js
+++ exercise.js
@@ -8,6 +8,16 @@ c     22      3
 
 
 // your code 
-const players = 
+const players = [
+  {
+    name: 'a', age: 23, rating: 5
+  },
+  {
+    name: 'b', age: 22, rating: 3
+  },
+  {
+    name: 'c', age: 22, rating: 3
+  }
+]
 // write single line code to print the 2nd player name in the list ( without using dot notation .name )
-console.log(.............)
+console.log(players[1]['name'])
