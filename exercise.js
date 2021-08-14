b11da385847c202ba77eb23aa30d612d5a8cf8f3
Mohammed yahya <43484164+m7md2281@users.noreply.github.com>
2021-08-14 14:43:05 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -8,6 +8,11 @@ c     22      3
 
 
 // your code 
-const players = 
+const players = [
+     {player1 : ['a', 23, 5]},
+     {player2 : ['b', 22, 3]},
+     {player3 : ['c', 22, 3]}
+]
+
 // write single line code to print the 2nd player name in the list ( without using dot notation .name )
-console.log(.............)
+console.log(players[1])
