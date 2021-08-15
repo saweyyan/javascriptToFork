6412091daeb565a1a4646a6c4fbb12695450e15a
huthaifaTayeh <huthaifazmi@gmail.com>
2021-08-15 07:33:09 +0300

    fixed errors in errors1.js and wrote exercise in exersice.js

--- error1.js
+++ error1.js
@@ -9,18 +9,19 @@ console.log('b is ',b)
 
 
 const x = 5
-/* x = 4  
-  const variables can't be reassigned */
+// x = 4  
+// const cannot be changed 
 console.log('x is ',x) 
 
 const d = 7
-/* d = 7  
-  const variables must have a value in the same declaration code */
+// d = 7  
+// const cannot be changed 
 console.log('d value is ',d) 
 
 
 /* console.log('y is ',y) 
   Y is not declared */
-
+let y = 11
+console.log('y value is ',y)
 // run the code, then comment the code that causing the error with explain short words , then fix the code 
 // * to comment the code add // in the start of the line or wrap the code with /* your code here */
